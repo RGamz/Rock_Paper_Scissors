33368bcd3a0e664dbd9e419dcda6030326b538bb
RGamz_PC <rgamzat@live.com>
2023-10-04 23:07:34 +0200

    functionality: added working onclick user input

--- script.js
+++ script.js
@@ -11,63 +11,72 @@ function getComputerChoice() {
     return item;
 }
 
-
-function getPlayerSelection() {
-    let input = prompt("Please make your choice between Rock, Paper or Scissors").toLowerCase()
-    return input
-}
-
-
 let computerChoice = null;
 let playerChoice = null;
 
-let computerScore = 0;
-let playerScore = 0;
+const buttonsDiv = document.querySelector(".buttonsDiv");
 
-function playRound(computerChoice,playerChoice) {
-
-    computerChoice = getComputerChoice();
-    playerChoice = getPlayerSelection();
-    
-    if (computerChoice == "rock" && playerChoice == "scissors") {
-        console.log("You Lose! Rock beats Scissors")
-        return computerScore++;
-    } else if (computerChoice == "scissors" && playerChoice == "rock") {
-        console.log("You Win! Rock beats Scissors")
-        return playerScore++;
-    } else if (computerChoice == "rock" && playerChoice == "paper") {
-        console.log("You Win! Paper beats Rock")
-        return playerScore++;
-    } else if (computerChoice == "paper" && playerChoice == "rock") {
-        console.log("You Lose! Paper beats Rock")
-        return computerScore++;
-    } else if (computerChoice == "scissors" && playerChoice == "paper") {
-        console.log("You Lose! Scissors beats Paper")
-        return computerScore++;
-    } else if (computerChoice == "paper" && playerChoice == "scissors") {
-        console.log("You Win! Scissors beats Paper")
-        return playerScore++;
-    } else {
-        console.log("It is a draw!")
+let playerSelection;
+buttonsDiv.addEventListener("click", function(event) {
+    if(event.target.nodeName === "DIV") {
+        return;
     }
-}
+    playerSelection = event.target.textContent.toLowerCase();
+    
 
-function game() {
-    while (true ) {
-        playRound()
-        console.log("Comp: " + computerScore)
-        console.log("Player: " + playerScore)
-
-        if (computerScore >= 3 || playerScore >= 3) {
-            break; 
+    let computerScore = 0;
+    let playerScore = 0;
+    
+    function playRound(computerChoice,playerChoice) {
+    
+        computerChoice = getComputerChoice();
+        playerChoice = playerSelection;
+        console.log(computerChoice)
+        console.log(playerSelection)
+       
+        if (computerChoice == "rock" && playerChoice == "scissors") {
+            console.log("You Lose! Rock beats Scissors")
+            return computerScore++;
+        } else if (computerChoice == "scissors" && playerChoice == "rock") {
+            console.log("You Win! Rock beats Scissors")
+            return playerScore++;
+        } else if (computerChoice == "rock" && playerChoice == "paper") {
+            console.log("You Win! Paper beats Rock")
+            return playerScore++;
+        } else if (computerChoice == "paper" && playerChoice == "rock") {
+            console.log("You Lose! Paper beats Rock")
+            return computerScore++;
+        } else if (computerChoice == "scissors" && playerChoice == "paper") {
+            console.log("You Lose! Scissors beats Paper")
+            return computerScore++;
+        } else if (computerChoice == "paper" && playerChoice == "scissors") {
+            console.log("You Win! Scissors beats Paper")
+            return playerScore++;
+        } else {
+            console.log("It is a draw!")
         }
     }
 
-    if (computerScore > playerScore) {
-        return "You lose!"
-    } else {
-        return "You won!"
+    function game() {
+        while (true ) {
+            playRound()
+            console.log("Comp: " + computerScore)
+            console.log("Player: " + playerScore)
+    
+            if (computerScore >= 3 || playerScore >= 3) {
+                break; 
+            }
+        }
+    
+        if (computerScore > playerScore) {
+            return "You lose!"
+        } else {
+            return "You won!"
+        }
     }
-}
+    
+    alert(game())
+})
 
-alert(game())
+
+
